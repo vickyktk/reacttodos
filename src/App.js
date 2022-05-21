047ee1827dfe0fc01461c8df1c4c3a0d9083ee55
vickyktk <waqasktk81@gmail.com>
2022-05-21 21:08:33 +0500

    Ready for production

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import Todo from './components/Todo'
-import Shophere from './components/ShopHere'
 import './index.css'
 
 function App(){
-	// fetch('http://localhost:3000/api')
-	// .then(res => res.json())
- //  	.then(result=>console.log(result))
   return(
-  	
     <div>
       <Todo />
     </div>
